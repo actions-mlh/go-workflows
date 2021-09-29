9d1302ae791823b27136c0237dfa34a3678ce92c
jjayeon <autumn.chiu.136@gmail.com>
2021-09-29 15:37:42 -0400

    read yaml file into map

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
 	fmt.Println("hello world!")
+
+	data, err := os.ReadFile("simple.yaml")
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+	fmt.Println(data)
+
+	obj := make(map[interface{}]interface{})
+	yaml.Unmarshal(data, obj)
+	fmt.Println(obj)
 }
