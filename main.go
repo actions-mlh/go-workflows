31dd10c690f0d79b1ac9bccc5d68e846e8ea9bcf
jjayeon <autumn.chiu.136@gmail.com>
2021-10-01 17:31:32 -0400

    minor refactor in main

--- main.go
+++ main.go
@@ -15,31 +15,24 @@ func main() {
 	args := flag.Args()
 
 	if len(args) == 0 {
-		data, err := io.ReadAll(os.Stdin)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		_, err = parser.Parse(data)
-		if err != nil {
-			log.Fatalf("error parsing stdin:\n    %v", err)
-		}
-
-		fmt.Println("passed!")
+		process(io.ReadAll(os.Stdin))
 	}
 
 	for _, arg := range args {
-		data, err := os.ReadFile(arg)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		_, err = parser.Parse(data)
-		if err != nil {
-			log.Fatalf("error parsing %v:\n    %v", arg, err)
-		}
-
-		// success case?
-		fmt.Println("passed!")
+		process(os.ReadFile(arg))
 	}
+}
+
+func process(data []byte, err error) error {
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = parser.Parse(data)
+	if err != nil {
+		log.Fatalf("error parsing stdin:\n    %v", err)
+	}
+
+	fmt.Println("passed!")
+	return nil
 }
