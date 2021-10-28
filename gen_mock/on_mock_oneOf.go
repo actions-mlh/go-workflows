25c8082c1bb6f5a5e860e1b247627cae18f68b11
jjayeon <autumn.chiu.136@gmail.com>
2021-10-28 17:37:36 -0400

    refactor On and Jobs

--- gen_mock/on_mock_oneOf.go
+++ gen_mock/on_mock_oneOf.go
@@ -98,15 +98,13 @@ func (node WorkflowNode) Lint(sink *lint.ProblemSink) error {
 	}
 	if node.Value.On == nil {
 		sink.Record(node.Raw, "%d:%d  error  missing on", node.Raw.Line, node.Raw.Column)
-	}
-	if node.Value.Defaults == nil {
-		sink.Record(node.Raw, "%d:%d  error  missing defaults", node.Raw.Line, node.Raw.Column)
-	}
-	if node.Value.Concurrency == nil {
-		sink.Record(node.Raw, "%d:%d  error  missing concurrency", node.Raw.Line, node.Raw.Column)
+	} else {
+		node.Value.On.Lint(sink)
 	}
 	if node.Value.Jobs == nil {
 		sink.Record(node.Raw, "%d:%d  error  missing jobs", node.Raw.Line, node.Raw.Column)
+	} else {
+		node.Value.Jobs.Lint(sink)
 	}
 	return nil
 }
@@ -153,9 +151,6 @@ func (node *WorkflowOnNode) UnmarshalYAML(value *yaml.Node) error {
 		// handles -> "configuration" -> "string", "number", "boolean"
 		// output.go -> create array called scalars := []{"string", "number", "boolean"}
 		// !!str, !!float, !!int
-		if node.Raw.Tag != "!!str" {
-			return fmt.Errorf("%d:%d  error  Expected one of: string type", node.Raw.Line, node.Raw.Column)
-		}
 		return value.Decode(&node.OneOf.ScalarNode)
 	case yaml.SequenceNode:
 		// will handle if key exists in linter for now, unless we decide to change
@@ -163,10 +158,6 @@ func (node *WorkflowOnNode) UnmarshalYAML(value *yaml.Node) error {
 	case yaml.MappingNode:
 		value := node.Raw
 
-		if len(value.Content)%2 != 0 {
-			// Uneven set of key value pairs (this shouldn't happen)
-			return fmt.Errorf("%d:%d  error  Expected even number of key value pairs", node.Raw.Line, node.Raw.Column)
-		}
 		event := new(WorkFlowOnValue)
 		for i := 0; i < len(value.Content); i += 2 {
 			keyEntry := value.Content[i]
@@ -206,6 +197,17 @@ func (node *WorkflowOnNode) UnmarshalYAML(value *yaml.Node) error {
 	}
 
 	// --- no required array field
+}
+
+func (node *WorkflowOnNode) Lint(sink *lint.ProblemSink) error {
+	if node.Raw.Kind == yaml.ScalarNode && node.Raw.Tag != "!!str" {
+		sink.Record(node.Raw, "%d:%d  error  Expected one of: string type", node.Raw.Line, node.Raw.Column)
+	}
+	if len(node.Raw.Content)%2 != 0 {
+		// Uneven set of key value pairs (this shouldn't happen)
+		sink.Record(node.Raw, "%d:%d  error  Expected even number of key value pairs", node.Raw.Line, node.Raw.Column)
+	}
+	return nil
 }
 
 type OnEventConstants string
@@ -276,6 +278,10 @@ func (node *OnCheckRunNode) UnmarshalYAML(value *yaml.Node) error {
 	default:
 		return fmt.Errorf("%d:%d  error  Expected one of: map type", node.Raw.Line, node.Raw.Column)
 	}
+}
+
+func (node *OnCheckRunNode) Lint(sink *lint.ProblemSink) error {
+	return nil
 }
 
 type OnCheckRunValue struct {
@@ -527,10 +533,6 @@ type WorkflowJobsPatternProperties struct {
 func (node *WorkflowJobsNode) UnmarshalYAML(value *yaml.Node) error {
 	node.Raw = value
 
-	if len(value.Content)%2 != 0 {
-		// Uneven set of key value pairs (this shouldn't happen)
-		return fmt.Errorf("%d:%d  error  Expected even number of key value pairs", node.Raw.Line, node.Raw.Column)
-	}
 	for i := 0; i < len(value.Content); i += 2 {
 		keyEntry := value.Content[i]
 
@@ -548,6 +550,14 @@ func (node *WorkflowJobsNode) UnmarshalYAML(value *yaml.Node) error {
 
 	}
 
+	return nil
+}
+
+func (node *WorkflowJobsNode) Lint(sink *lint.ProblemSink) error {
+	if len(node.Raw.Content)%2 != 0 {
+		// Uneven set of key value pairs (this shouldn't happen)
+		sink.Record(node.Raw, "%d:%d  error  Expected even number of key value pairs", node.Raw.Line, node.Raw.Column)
+	}
 	return nil
 }
 
