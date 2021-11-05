7e4595dd165aca289ff9990d334c27bd5059d703
jjayeon <autumn.chiu.136@gmail.com>
2021-11-05 11:58:25 -0400

    add testing for dirty files (not finished)

--- test/main_test.go
+++ test/main_test.go
@@ -46,4 +46,40 @@ func TestParse(t *testing.T) {
 
 		return nil
 	})
+
+	root = "../yaml/dirty/"
+	filepath.Walk(root, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() ||
+			!(filepath.Ext(path) == ".yml" ||
+				filepath.Ext(path) == ".yaml") {
+			return nil
+		}
+		input, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer input.Close()
+
+		expected, err := os.Open(path + ".exp")
+
+		sink := &sink.ProblemSink{Filename: path, Output: os.Stdout}
+		node := new(workflow.WorkflowNode)
+		
+		if err := yaml.NewDecoder(input).Decode(&node); err != nil {
+			return err
+		}
+
+		if err := lint.LintWorkflowRoot(sink, node); err != nil {
+			return err
+		}
+		if len(sink.Problems) > 0 {
+			t.Errorf("error(s) found in clean file %s:\n%s", path, strings.Join(sink.Problems, "\n"))
+		}
+
+		return nil
+	})
 }
